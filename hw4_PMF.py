93d788709ef983cb3558b1fd702314d8ee2236fb
LILIT <lilit555ai@gmail.com>
2018-09-21 15:10:14 -0600

    Add first part of PMF function

--- hw4_PMF.py
+++ hw4_PMF.py
@@ -8,3 +8,26 @@ lam = 2
 sigma2 = 0.1
 d = 5
 
+# Implement function here
+def PMF(train_data):
+    users = {}
+    objects = {}
+
+    for item in train_data:
+        u = int(item[0])-1
+        v = int(item[1])-1
+    
+        if u not in users:
+            users[u] = [v]
+        else:
+            users[u].append(v)
+        
+        if v in objects:
+            objects[v].append(u)
+        else:
+            objects[v] = [u]
+    #print(len(users))  
+    #print (len(objects))
+    #print(train_data.shape)
+    
+    
