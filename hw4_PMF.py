c9fcc8d4f0ee3e1cfe2f182b5ac6ac4e31d92d55
LILIT <lilit555ai@gmail.com>
2018-09-22 14:40:36 -0600

    Finished PMF function

--- hw4_PMF.py
+++ hw4_PMF.py
@@ -30,4 +30,58 @@ def PMF(train_data):
     #print (len(objects))
     #print(train_data.shape)
     
+    lam = 2
+    sigma2 = 0.1
+    d = 5
+    I = np.identity(5)
+    len_U = len(users)
+    len_V = len(objects)
+    M = np.zeros(shape = (len_U, len_V))        
+    U = np.zeros(shape = (50,len_U,5))  
+    V = np.zeros(shape = (50,len_V,5))
+    L = np.zeros(shape = (50,1))
     
+    for item in train_data:
+        M[int(item[0]-1)][int(item[1])-1] = item[2]
+    
+    #v_init = np.zeros(shape = (len_V,d))
+    for i in range(len(objects)):
+        V[0][i] = np.dot(np.random.normal(0,1/lam,d),I)
+    
+    for k in range(50):
+        for i in range(len_U):
+            v_sum = 0
+            m_sum = 0
+            for j in objects[i]:
+                v_sum += np.dot(V[k][j],V[k][j].reshape((-1,1)))
+                m_sum += M[i][j]*V[k][j]
+        U[k][i] = np.dot(np.linalg.inv(lam*sigma2*I + v_sum),m_sum)
+        
+        for j in range(len_V):
+            u_sum = 0
+            m_sum = 0
+            for i in objects[j]:
+                u_sum += np.dot(U[k][i],U[k][i].reshape((-1,1)))
+                m_sum += M[i][j]*U[k][i]
+            V[k][i] = np.dot(np.linalg.inv(lam*sigma2*I + u_sum),m_sum)
+    
+        m_sum = 0
+        v_sum = 0
+        u_sum = 0
+        for item in train_data:
+            ui = int(item[0])-1
+            vj = int(item[1])-1
+            m_sum += (item[2] - np.dot(U[k][ui],V[k][vj].reshape((-1,1))))**2
+        m_sum /= 2*sigma2
+    
+        for u in U[k]:
+            u_sum += np.linalg.norm(u)
+        u_sum = u_sum*lam/2
+    
+        for v in V[k]:
+            v_sum += np.linalg.norm(v)
+        v_sum = v_sum*lam/2
+    
+        L[k] = L[k] - m_sum - u_sum - v_sum
+    
+    return L, U, V
